d8b0294f00b082ad33bfcbc433e1ec928ede22ef
juba0713 <basasrepo@gmail.com>
2024-11-23 14:35:59 +0800

    Push RoomList Screen in User

---
+++ ABSystem.WebApp/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABSystem.WebApp.Controllers
+{
+    [Authorize]
+    public class BookController : Controller
+    {
+        [HttpGet]
+        [Route("/book")]
+        public IActionResult BookingListScreen()
+        {
+            return PartialView("~/Views/User/RoomList.cshtml");
+        }
+    }
+}
